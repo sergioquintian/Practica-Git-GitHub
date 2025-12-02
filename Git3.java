a4482c255525ca122f93188d11de09d677469c1d
Sergio Quintian Garcia <sergioqg17@educastur.es>
2025-12-02 19:38:47 +0100

    Añadida clase Calculadora Git3

---
+++ Git3.java
@@ -0,0 +1,21 @@
+
+public class Git3 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        boolean estaVacia=false;
+        int edad=26;
+        double facturaLuz=230.56;
+        char estadoCivil='s';
+        String nombreAsignatura="Programacion";
+        double areaFigura=23.45;
+        boolean estaAprobado=true;
+
+     System.out.println("el área de la figura es "+areaFigura+" y mi estado civil es "+estadoCivil);
+
+
+
+
+    }
+}
